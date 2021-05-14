8ccf3dc09be8b2db73f772707b68cd3503530ab4
Benaiah Mischenko <benaiah@mischenko.com>
2021-05-13 18:15:14 -0700

    Log @netlify/config output

--- cancel-every-deploy-plugin/index.js
+++ cancel-every-deploy-plugin/index.js
@@ -1,5 +1,6 @@
 module.exports = {
-  onPostBuild({ utils: { build: { cancelBuild } } }) {
+  onPostBuild({ utils: { build: { cancelBuild } }, netlifyConfig }) {
+    console.log(JSON.stringify(netlifyConfig, null, 2))
     cancelBuild('automatically canceling build!')
   }
 }
